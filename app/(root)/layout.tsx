61db636b0128a33782991d0d4e568ebce6d41def
Nikhil123V <128300239+Nikhil123V@users.noreply.github.com>
2024-06-17 02:16:48 +0530

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,25 @@
+import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next';
+import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: "ConnectHub",
+  description: "Video Calling app",
+  icons:{
+    icon:'/icons/logo.png',
+  }
+};
+
+const RootLayout = ({ children }:{children:ReactNode}) => {
+    
+  return (
+    <main>
+        <StreamVideoProvider>
+        {children}
+        </StreamVideoProvider>
+        
+    </main>
+  )
+}
+
+export default RootLayout
